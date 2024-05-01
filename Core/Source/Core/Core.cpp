3e2bbb580b6b11bcfb9614e933b07a9cae75bd0e
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2024-05-01 23:01:41 +0200

    Added examples for platform defines in Core.cpp

--- Core/Source/Core/Core.cpp
+++ Core/Source/Core/Core.cpp
@@ -6,8 +6,33 @@ namespace Core {
 
 	void PrintHelloWorld()
 	{
-		std::cout << "Hello World!\n";
+#ifdef DEBUG
+		std::cout << "Running in Debug Configuration!\n";
+#endif
+
+#ifdef RELEASE
+		std::cout << "Running in Release Configuration!\n";
+#endif
+
+#ifdef DIST
+		std::cout << "Running in Dist Configuration!\n";
+#endif
+
+#ifdef WINDOWS
+		std::cout << "Hello World from Windows!\n";
 		std::cin.get();
+#endif
+
+#ifdef LINUX
+		std::cout << "Hello World from Linux!\n";
+		std::cin.get();
+#endif
+
+#ifdef MACOSX
+		std::cout << "Hello World from MacOSX!\n";
+		std::cin.get();
+#endif
+		
 	}
 
 }
